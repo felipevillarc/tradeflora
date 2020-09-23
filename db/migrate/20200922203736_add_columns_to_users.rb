7ff4260fef25b82a651fd446bc7b2bd934247e7c
Otoniel Miranda <ogmfilho@gmail.com>
2020-09-23 16:36:21 -0300

    type attribute in Users table changed to user_type; Seed file updated (Trades table missing)

--- db/migrate/20200922203736_add_columns_to_users.rb
+++ db/migrate/20200922203736_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :type, :string
+    add_column :users, :user_type, :string
     add_column :users, :name, :string
     add_column :users, :document_number, :string
     add_column :users, :phone_number, :string
