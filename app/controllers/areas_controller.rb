ce2078215ccb9ab6fcb5dd308e89d831c97eee4e
luis cavalcanti <felipevillarc@hotmail.com>
2020-09-23 16:16:48 -0300

    create do AreaController e form da página new de area

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -20,25 +20,41 @@ class AreasController < ApplicationController
   def create
     @area = Area.new(area_params)
     @area.user = current_user
-
-    redirect_to @area, notice: 'Nova área criada.' 
+    
+    if @area.save!
+      redirect_to area_path(@area), notice: 'Nova área criada.'
+    else
+      @basins = Basin.all
+      render :new
+    end
   end
-
+  
   def update
     @area = Area.find(params[:id])
     @area.user = current_user
   end
-
+  
   def destroy
     @area = Area.find(params[:id])
     @area.destroy
-
+    
     redirect_to @area, notice: 'Área removida.' 
   end
-
+  
   private
-
+  
   def area_params
-    params.require(:area).permit(:lat, :long, :description, :basin)
+    params.require(:area).permit(:lat, :long, :description, :basin_id)
   end
 end
+"""
+def create 
+  @area = Area.new(area_params) 
+  @user = current_user 
+  @area.user = @user
+  if @area.save 
+  redirect_to '/'
+  else
+    raise
+  end
+end"""
