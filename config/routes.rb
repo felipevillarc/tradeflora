9fd5cb6a473b99e8a307c42ecd14a8e2cb7e1cd1
watila machado <watilapm@gmail.com>
2020-09-28 16:28:13 -0300

    Função aprovar funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :areas do
+    patch "aprove", to: "areas#aprove" 
   	resources :trades, only: [ :new, :create, :destroy ]
   end
 
